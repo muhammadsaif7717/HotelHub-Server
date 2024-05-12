ee770629073c65f2572322f6a4de8173eba26943
MuHammad Saif <muhammadsaif7717@gmail.com>
2024-05-12 10:45:31 +0600

    User Posting functionality added

--- index.js
+++ index.js
@@ -1,14 +1,56 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser')
 const dotenv = require("dotenv");
 dotenv.config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
+
+//custom midleweres
+const logger = async (req, res, next) => {
+  console.log('Called', req.host, req.originalUrl)
+  next();
+}
+
+const verifyToken = async (req, res, next) => {
+  const token = req.cookies?.token;
+  // console.log('Value of the token in middlewere',token)
+  if (!token) {
+    return res.status(401).send({ message: 'unauthorized access' })
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      console.log(err)
+      return res.status(401).send({ message: 'unauthorized access' })
+    }
+    // console.log('value in the token', decoded);
+    req.user = decoded;
+    next();
+  })
+}
+
+
+
 //middlewere's
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser());
+
+
+
+
+
+
+
+
+
+
 
 // manage detabase
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oh0s98i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -29,12 +71,58 @@ async function run() {
 
     //collections
     const userCollection = client.db("hotelHubDB").collection("users");
+    const roomsCollection = client.db("hotelHubDB").collection("rooms");
 
-    //operations
-    app.get('/users', async (req, res) => {
-      const users = await userCollection.find({}).toArray();
-      res.send(users);
+    //?!operations//
+
+    //AUTH related API
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+      res
+        .cookie('token', token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === 'production',
+          sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+
+        })
+        .send({ success: true })
     })
+
+
+
+
+    // POST users
+    app.post("/users", async (req, res) => {
+      const newUser = req.body;
+      const { email } = newUser;
+      // Check if user already exists
+      const existingUser = await userCollection.findOne({ email });
+      if (existingUser) {
+        return res.send(console.log('User already exists'));
+      }
+      else {
+        res.send(await userCollection.insertOne(newUser));
+      }
+    });
+
+
+    // GET users by email
+    app.get("/users/:email", async (req, res) => {
+      res.send(await userCollection.find(req.params.email).toArray());
+    });
+
+    // GET all users
+    app.get('/users', async (req, res) => {
+      res.send(await userCollection.find({}).toArray());
+    });
+
+    // GET all rooms
+    app.get('/rooms', async (req, res) => {
+      res.send(await roomsCollection.find({}).toArray());
+    });
+
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });  //comment this line before deploy in varcel
